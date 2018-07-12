13ab42a7318519d39020f29e39d0799217511498
iD Student <depianoman2004@gmail.com>
2018-07-12 11:25:31 -0400

    Test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pokeapi',
+      version='0.3',
+      description='A Pokemon API for Python!',
+      url='https://www.github.com/DePianoman',
+      author='DePianoman',
+      author_email='depianoman2004@gmail.com',
+      license='MIT',
+      packages=['pokeapi'],
+      zip_safe=False)
